d0370bb223fbe735f7c2f3cce23c6825b1b2be6e
TanMusong <tanmusong@gmail.com>
2022-10-12 16:46:48 +0800

    feat creator uuid & base64 转换

--- dist/CCT.js
+++ dist/CCT.js
@@ -1,2 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const UUID_1 = require("./uuid/UUID");
+var cct;
+(function (cct) {
+    cct.uuidToBase64 = (uuid) => {
+        return (0, UUID_1.UUIDToBase64)(uuid);
+    };
+    cct.base64ToUUID = (base64) => {
+        return (0, UUID_1.Base64ToUUID)(base64);
+    };
+})(cct || (cct = {}));
+exports.default = cct;
--- dist/CCTCMD.js
+++ dist/CCTCMD.js
@@ -8,9 +8,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const CCT_1 = __importDefault(require("./CCT"));
 const runner = () => __awaiter(void 0, void 0, void 0, function* () {
     const command = process.argv[2];
     switch (command) {
+        case 'u2b':
+            {
+                const uuid = process.argv[3];
+                console.log(uuid ? CCT_1.default.uuidToBase64(uuid) : 'Empty UUID');
+            }
+            break;
+        case 'b2u':
+            {
+                const base64 = process.argv[3];
+                console.log(base64 ? CCT_1.default.base64ToUUID(base64) : 'Empty Base64');
+            }
+            break;
         default:
             console.error(`Error Command: ${command}`);
             process.exit(1);
