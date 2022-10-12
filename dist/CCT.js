12d2c52fde3aa85379f96e8658594122ed10e300
TanMusong <tanmusong@gmail.com>
2022-10-12 17:00:52 +0800

    feat creator js 加密

--- dist/CCT.js
+++ dist/CCT.js
@@ -1,5 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const pako_1 = __importDefault(require("pako"));
+const xxtea_node_1 = __importDefault(require("xxtea-node"));
 const UUID_1 = require("./uuid/UUID");
 var cct;
 (function (cct) {
@@ -8,6 +13,22 @@ var cct;
     };
     cct.base64ToUUID = (base64) => {
         return (0, UUID_1.Base64ToUUID)(base64);
+    };
+    cct.decryptJSC = (data, key, compress = true) => {
+        const byteKey = xxtea_node_1.default.toBytes(key);
+        let decryptData = xxtea_node_1.default.decrypt(data, byteKey);
+        if (!decryptData)
+            return null;
+        if (compress)
+            decryptData = pako_1.default.ungzip(decryptData);
+        return xxtea_node_1.default.toString(decryptData);
+    };
+    cct.encryptJS = (data, key, compress = true) => {
+        const byteKey = xxtea_node_1.default.toBytes(key);
+        let byteData = xxtea_node_1.default.toBytes(data);
+        if (compress)
+            byteData = pako_1.default.gzip(byteData, { level: 6 });
+        return xxtea_node_1.default.encrypt(byteData, byteKey);
     };
 })(cct || (cct = {}));
 exports.default = cct;
--- src/CCTCMD.ts
+++ src/CCTCMD.ts
@@ -1,4 +1,17 @@
 import cct from "./CCT";
+import fs from "fs";
+
+const getArg = (key: string, check?: (value: string) => boolean): string | null => {
+    const index = process.argv.indexOf(key);
+    if (index < 0) return null;
+    const value = process.argv[index + 1];
+    return check ? (check(value) ? value : null) : value;
+}
+
+const haveArg = (key: string): boolean => {
+    const index = process.argv.indexOf(key);
+    return index >= 0;
+}
 
 const runner = async (): Promise<void> => {
 
@@ -14,6 +27,32 @@ const runner = async (): Promise<void> => {
             {
                 const base64 = process.argv[3];
                 console.log(base64 ? cct.base64ToUUID(base64) : 'Empty Base64');
+            }
+            break;
+        case 'd':
+        case 'decrypt':
+        case 'e':
+        case 'encrypt':
+            {
+                const filePath = process.argv[3];
+                if (!fs.existsSync(filePath)) {
+                    console.error(`${filePath} not exists`);
+                    process.exit(1);
+                }
+                const xxtea = getArg('-xxtea', value => !value.startsWith('-'));
+                if (!xxtea) {
+                    console.error(`illegal parameter: -xxtea`);
+                    process.exit(1);
+                }
+                const outFilePath = getArg('-out', value => !value.startsWith('-')) || filePath;
+                const compress = haveArg('-compress') || haveArg('-zip');
+                const cotnent = fs.readFileSync(filePath);
+                const script: string | Uint8Array | null = (command === 'd' || command === 'decrypt') ? cct.decryptJSC(cotnent, xxtea, compress) : cct.encryptJS(cotnent.toString(), xxtea, compress);
+                if (!script) {
+                    console.error(`xxtea error`);
+                    process.exit(2);
+                }
+                fs.writeFileSync(outFilePath, script);
             }
             break;
         default:
